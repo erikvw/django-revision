ddb68fee09890e3669210eaef6a53b8c0dedf154
erik van widenfelt <ew2789@gmail.com>
2015-05-20 10:14:38 +0200

    get max_length from revision.py, cleanup revision.py

--- revision/revision.py
+++ revision/revision.py
@@ -10,7 +10,62 @@ class Revision(object):
         self._repo = None
         self._tag = None
         self._working_dir = working_dir
-        self._revision = self.manual_revision
+        self._revision = None
+        self.max_length = 75
+
+    def __repr__(self):
+        return '{0}({1.working_dir!r}, {1.manual_revision!r})'.format(self.__class__.__name__, self)
+
+    def __str__(self):
+        return '{0.revision!s}'.format(self)
+
+    @property
+    def repo(self):
+        """Returns a git repo object."""
+        if not self._repo:
+            try:
+                self._repo = Repo(self.working_dir, odbt=GitDB)
+            except InvalidGitRepositoryError as e:
+                raise InvalidGitRepositoryError(
+                    'Invalid git repo. Got \'{}\'. The git \'working_dir\' '
+                    'must the root of the git repo. Try setting GIT_DIR in settings.'.format(e))
+        return self._repo
+
+    @property
+    def revision(self):
+        """Returns a revision string of tag:branch:commit."""
+        if not self._revision:
+            self._revision = self.manual_revision
+            if not self._revision:
+                try:
+                    self.branch = str(self.repo.active_branch)
+                    self.commit = str(self.repo.active_branch.commit)
+                    self._revision = '{0}:{1}'.format(self.branch, self.commit)
+                except TypeError:
+                    self.branch = 'detached'
+                    self.commit = str(self.repo.commit())
+                self._revision = '{}:{}:{}'.format(self.tag, self.branch, self.commit)
+        return self._revision[0:self.max_length]
+
+    @property
+    def manual_revision(self):
+        """Returns a manually set revision string, e.g set by the user in settings.REVISION."""
+        if self._manual_revision is None:
+            try:
+                self._manual_revision = settings.REVISION
+            except AttributeError:
+                pass
+        return self._manual_revision
+
+    @property
+    def tag(self):
+        """Returns the tag from git describe."""
+        if self._tag is None:
+            try:
+                self._tag = self.repo.git.describe(tags=True)
+            except GitCommandError:
+                self._tag = ''
+        return self._tag
 
     @property
     def working_dir(self):
@@ -27,60 +82,5 @@ class Revision(object):
                         'Missing settings attribute \'BASE_DIR\' or \'GIT_DIR\' '
                         'required by django-revision.')
         return self._working_dir
-
-    @property
-    def manual_revision(self):
-        """Returns a manually set revision string, e.g set by the user in settings.REVISION."""
-        if self._manual_revision is None:
-            try:
-                self._manual_revision = settings.REVISION
-                self.revision = self._manual_revision
-            except AttributeError:
-                self._manual_revision = None
-        return self._manual_revision
-
-    @property
-    def revision(self):
-        """Returns a revision string of tag:branch:commit."""
-        if not self._revision:
-            self._revision = self.manual_revision
-            if not self._revision:
-                try:
-                    self.branch = str(self.repo.active_branch)
-                    self.commit = str(self.repo.active_branch.commit)
-                    self.revision = '{0}:{1}'.format(self.branch, self.commit)
-                except TypeError:
-                    self.branch = 'detached'
-                    self.commit = str(self.repo.commit())
-                self._revision = '{}:{}:{}'.format(self.tag, self.branch, self.commit)
-        return self._revision
-
-    @property
-    def tag(self):
-        """Returns the tag from git describe."""
-        if self._tag is None:
-            try:
-                self._tag = self.repo.git.describe(tags=True)
-            except GitCommandError:
-                self._tag = ''
-        return self._tag
-
-    @property
-    def repo(self):
-        """Returns a git repo object."""
-        if not self._repo:
-            try:
-                self._repo = Repo(self.working_dir, odbt=GitDB)
-            except InvalidGitRepositoryError as e:
-                raise InvalidGitRepositoryError(
-                    'Invalid git repo. Got \'{}\'. The git \'working_dir\' '
-                    'must the root of the git repo. Try setting GIT_DIR in settings.'.format(e))
-        return self._repo
-
-    def __repr__(self):
-        return '{0}({1.tag!r})'.format(self.__class__.__name__, self)
-
-    def __str__(self):
-        return '{0.tag!s}'.format(self)
 
 site_revision = Revision()
