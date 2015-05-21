f4ef5c85b2ddd89a16ce4e5ae4dc7c77a13d43fe
erik van widenfelt <ew2789@gmail.com>
2015-05-21 14:59:24 +0200

    setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-revision',
-    version='0.1.6a0',
+    version='0.1.6a1',
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
@@ -22,6 +22,9 @@ setup(
     long_description=README,
     zip_safe=False,
     keywords='django fields git',
+    install_requires=[
+        'GitPython>=1.0',
+    ],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
