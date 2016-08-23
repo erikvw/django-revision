8e651b64cb8cece3af0775458490386bc91401a4
erikvw <ew2789@gmail.com>
2016-08-23 22:27:50 +0200

    fix view

--- django_revision/views.py
+++ django_revision/views.py
@@ -4,7 +4,7 @@ from django_revision.revision import Revision
 class RevisionMixin:
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super(RevisionMixin, self).get_context_data(**kwargs)
         revision = Revision()
         context.update({
             'revision': revision.tag,
