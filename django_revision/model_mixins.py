96734280a0a358c16b7a6a187d6a3ca2de1707e0
erikvw <ew2789@gmail.com>
2025-07-21 07:33:38 -0500

    update setup, drop support for dj4 and anything lower than py312

--- django_revision/model_mixins.py
+++ django_revision/model_mixins.py
@@ -5,7 +5,9 @@ from .revision_field import RevisionField
 
 class RevisionModelMixin(models.Model):
 
-    revision = RevisionField(help_text="System field. Git repository tag:branch:commit.")
+    revision = RevisionField(
+        help_text="System field. Git repository tag:branch:commit."
+    )
 
     class Meta:
         abstract = True
